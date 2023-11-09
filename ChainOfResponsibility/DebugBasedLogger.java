5100ae3a6a88af2f6919f47e248eac1e1085f57b
Anjum Khan <55983582+anjum04@users.noreply.github.com>
2023-11-09 10:59:18 +0530

    Add files via upload

---
+++ ChainOfResponsibility/DebugBasedLogger.java
@@ -0,0 +1,13 @@
+package ChainOfResponsibility;
+
+public class DebugBasedLogger extends Logger{
+	
+	public DebugBasedLogger(int levels) {  
+        this.levels=levels;  
+    }  
+    @Override  
+    protected void displayLogInfo(String msg) {  
+        System.out.println("DEBUG LOGGER INFO: "+msg);  
+    }  
+
+}
